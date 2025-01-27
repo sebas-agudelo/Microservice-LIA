e0577de5a77a5634c5f952156a6419e828fbb39f
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-01-27 13:51:27 +0100

    Added express

--- reports-dashboard-service/src/app.js
+++ reports-dashboard-service/src/app.js
@@ -1,3 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servern är igång på port ${process.env.PORT}`);
+    
+})
+
+
+
+
+
